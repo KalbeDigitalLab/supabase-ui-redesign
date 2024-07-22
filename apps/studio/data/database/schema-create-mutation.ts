009ce0fbb8f112c156f71b323200478931601d7f
JeremyMic <mexjoe34@gmail.com>
2024-07-22 14:00:39 +0700

    Revert Commit to 5e5586d

--- apps/studio/data/database/schema-create-mutation.ts
+++ apps/studio/data/database/schema-create-mutation.ts
@@ -13,12 +13,19 @@ export type SchemaCreateVariables = {
 }
 
 export async function createSchema({ name, projectRef, connectionString }: SchemaCreateVariables) {
-  const sql = pgMeta.schemas.create({ name, owner: 'postgres' }).sql
-  const { result } = await executeSql({
-    projectRef,
-    connectionString,
-    sql,
-    queryKey: ['schema', 'create'],
+  if (!projectRef) throw new Error('projectRef is required')
+  if (!connectionString) throw new Error('Connection string is required')
+
+  let headers = new Headers()
+  headers.set('x-connection-encrypted', connectionString)
+
+  const { data, error } = await post('/platform/pg-meta/{ref}/schemas', {
+    params: {
+      header: { 'x-connection-encrypted': connectionString },
+      path: { ref: projectRef },
+    },
+    body: { name, owner: 'postgres' },
+    headers: Object.fromEntries(headers),
   })
   return result
 }
